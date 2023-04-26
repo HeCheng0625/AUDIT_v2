0574d4b6a3a57974ea51cd0a7275ceddbf196bbf
yuanchengwang <1057041679@qq.com>
2023-04-26 08:11:06 +0000

    commit

--- train_audit_g.sh
+++ train_audit_g.sh
@@ -1,4 +1,4 @@
-export MODEL_NAME="/blob/v-yuancwang/AudioEditingModel/Diffusion_SG/checkpoint-350000"
+export MODEL_NAME="/blob/v-yuancwang/AudioEditingModel/Diffusion_SG/checkpoint-10000"
 export TRAIN_DIR=""
 
 accelerate launch train_audit_g.py \
@@ -6,12 +6,12 @@ accelerate launch train_audit_g.py \
   --train_data_dir=$TRAIN_DIR \
   --use_ema \
   --resolution=512 --center_crop --random_flip \
-  --train_batch_size=4 \
+  --train_batch_size=6 \
   --gradient_accumulation_steps=1 \
   --gradient_checkpointing \
   --max_train_steps=1000000 \
-  --checkpointing_steps=100 \
-  --learning_rate=9.6e-5 \
+  --checkpointing_steps=2000 \
+  --learning_rate=2e-5 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
-  --output_dir="/blob/v-yuancwang/AUDITPLUS/AUDIT_G"
+  --output_dir="/blob/v-yuancwang/AUDITPLUS/AUDIT_G" \
