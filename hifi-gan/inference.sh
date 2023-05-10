1a5c18901648fa132a2121b61e10f9c5a51ed650
yuanchengwang <119010319@link.cuhk.edu.cn>
2023-05-10 10:03:30 +0000

    commit

--- hifi-gan/inference.sh
+++ hifi-gan/inference.sh
@@ -1,4 +1,34 @@
 /opt/conda/envs/control/bin/python /home/v-yuancwang/AUDIT_v2/hifi-gan/inference_e2e.py \
---input_mels_dir="/home/v-yuancwang/AUDIT_v2/test_vae_mel" \
---output_dir="/home/v-yuancwang/AUDIT_v2/test_vae_wav" \
---checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_01250000"
+--input_mels_dir="/blob/v-yuancwang/audio_editing_test/generate/350000/7.0/mel" \
+--output_dir="/blob/v-yuancwang/audio_editing_test/generate/350000/7.0/wav" \
+--checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_01000000"
+
+# /opt/conda/envs/control/bin/python /home/v-yuancwang/AUDIT_v2/hifi-gan/inference_e2e.py \
+# --input_mels_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/450000_5.5/mel" \
+# --output_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/450000_5.5/wav" \
+# --checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_01450000"
+
+# /opt/conda/envs/control/bin/python /home/v-yuancwang/AUDIT_v2/hifi-gan/inference_e2e.py \
+# --input_mels_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/400000_7.5/mel" \
+# --output_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/400000_7.5/wav" \
+# --checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_01450000"
+
+# /opt/conda/envs/control/bin/python /home/v-yuancwang/AUDIT_v2/hifi-gan/inference_e2e.py \
+# --input_mels_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/350000_7.5/mel" \
+# --output_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/350000_7.5/wav" \
+# --checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_01450000"
+
+# /opt/conda/envs/control/bin/python /home/v-yuancwang/AUDIT_v2/hifi-gan/inference_e2e.py \
+# --input_mels_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/300000_7.5/mel" \
+# --output_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/300000_7.5/wav" \
+# --checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_01450000"
+
+# /opt/conda/envs/control/bin/python /home/v-yuancwang/AUDIT_v2/hifi-gan/inference_e2e.py \
+# --input_mels_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/400000_6.5/mel" \
+# --output_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/400000_6.5/wav" \
+# --checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_01450000"
+
+# /opt/conda/envs/control/bin/python /home/v-yuancwang/AUDIT_v2/hifi-gan/inference_e2e.py \
+# --input_mels_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/430000_7.5/mel" \
+# --output_dir="/blob/v-yuancwang/AUDITDATA/AUDIT_INFER/430000_7.5/wav" \
+# --checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_01450000"
