5503ddff8bfe89a4ccbca352fc7dc1aa76384aeb
yuanchengwang <119010319@link.cuhk.edu.cn>
2023-05-13 03:40:01 +0000

    commit

--- hifi-gan/inference.sh
+++ hifi-gan/inference.sh
@@ -1,6 +1,6 @@
 /opt/conda/envs/control/bin/python /home/v-yuancwang/AUDIT_v2/hifi-gan/inference_e2e.py \
---input_mels_dir="/home/v-yuancwang/AUDIT_v2/test_mel" \
---output_dir="/home/v-yuancwang/AUDIT_v2/test_wav" \
+--input_mels_dir="/home/v-yuancwang/AUDIT_v2/como/test_mel" \
+--output_dir="//home/v-yuancwang/AUDIT_v2/como/test_wav" \
 --checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_01250000"
 
 # /opt/conda/envs/control/bin/python /home/v-yuancwang/AUDIT_v2/hifi-gan/inference_e2e.py \
